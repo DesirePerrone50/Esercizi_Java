be497cbb09fd991eb087dacf73b483c9551345b9
DesirePerrone50 <perronedesire50@gmail.com>
2024-05-31 14:31:03 +0200

    Add files via upload

---
+++ ProfessorStudent/src/main/java/com/example/professorstudent/ProfessorStudentApplication.java
@@ -0,0 +1,13 @@
+package com.example.professorstudent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProfessorStudentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProfessorStudentApplication.class, args);
+	}
+
+}
---
+++ ProfessorStudent/src/test/java/com/example/professorstudent/ProfessorStudentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.professorstudent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProfessorStudentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
